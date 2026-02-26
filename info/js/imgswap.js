2a17fb66a8a67e99cdd1c0824cf3fdf5bc9060ee
nikunohi <sseanyagi@gmail.com>
2026-02-26 05:11:04 -0800

    adsasd

--- info/js/imgswap.js
+++ info/js/imgswap.js
@@ -1,19 +1,35 @@
+// console.log("imgswap.js loaded");
+// import cOne from 'url:../../images/b.svg';
+// import cTwo from 'url:../../images/b-2.svg';
+
+
+// setTimeout(function () {
+//     // document.querySelector('#blogo').style.color = "pink";
+
+// }, 3000);
+
+// document.querySelector('#blogo').onmouseenter = function () { 
+//     console.log('hover');
+//     document.querySelector('#blogo').src = cTwo;
+// };
+
+// document.querySelector('#blogo').onmouseleave = function () { 
+//     console.log('hover');
+//     document.querySelector('#blogo').src = cOne;
+// };
+
+
 console.log("imgswap.js loaded");
+
 import cOne from 'url:../../images/b.svg';
 import cTwo from 'url:../../images/b-2.svg';
 
+window.addEventListener('DOMContentLoaded', () => {
+  const blogo = document.querySelector('#blogo');
+  if (!blogo) return console.warn('#blogo not found');
 
-setTimeout(function () {
-    // document.querySelector('#blogo').style.color = "pink";
+  blogo.src = cOne; // set default image
 
-}, 3000);
-
-document.querySelector('#blogo').onmouseenter = function () { 
-    console.log('hover');
-    document.querySelector('#blogo').src = cTwo;
-};
-
-document.querySelector('#blogo').onmouseleave = function () { 
-    console.log('hover');
-    document.querySelector('#blogo').src = cOne;
-};
+  blogo.addEventListener('mouseenter', () => blogo.src = cTwo);
+  blogo.addEventListener('mouseleave', () => blogo.src = cOne);
+});
