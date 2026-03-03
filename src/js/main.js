d76bbc2818cb5e82085a34edd89c3f9529908727
nikunohi <sseanyagi@gmail.com>
2026-03-03 04:20:15 -0800

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
+console.log("asdasdsadasd");
+
 import '../css/altstyles.css';
 import '../info/js/imgswap.js';
 import '../info/js/grid.js';
 import '../info/js/barsInsttwo.js';
 
-console.log("asdasdsadasd");
