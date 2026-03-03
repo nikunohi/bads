c9a87185551d0791c45f1bb33581d6a393b06bc7
nikunohi <sseanyagi@gmail.com>
2026-03-03 03:27:40 -0800

    cleanse

--- docs/bads.fd33d70f.js
+++
@@ -1,2 +0,0 @@
-var e,l={};l=import.meta.resolve("2GMYw");let t=document.createElement("link");t.rel="icon",t.href=(e=l)&&e.__esModule?e.default:e,document.head.appendChild(t);
-//# sourceMappingURL=bads.fd33d70f.js.map
