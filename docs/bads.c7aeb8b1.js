95e701170efec113f3f2c18d2de4513ebeef65fc
nikunohi <sseanyagi@gmail.com>
2026-03-03 03:13:25 -0800

    changing structure

---
+++ docs/bads.c7aeb8b1.js
@@ -0,0 +1,2 @@
+var e,d={};d=import.meta.resolve("dx6hE");let l=document.createElement("link");l.rel="icon",l.href=(e=d)&&e.__esModule?e.default:e,document.head.appendChild(l);
+//# sourceMappingURL=bads.c7aeb8b1.js.map
