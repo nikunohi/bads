535a6f34971c20a06b23952fe1305b7c7e2f1b36
nikunohi <sseanyagi@gmail.com>
2026-03-03 18:25:59 -0800

    re,pve

--- src/info/index.js
+++ src/info/index.js
@@ -1,1 +1,8 @@
-import './main.js';
+
+
+import '../css/altstyles.css';
+import './js/imgswap.js';
+import './js/grid.js';
+import './js/barsInsttwo.js';
+
+console.log("asdasdsadasd");
--- src/info/main.js
+++
@@ -1,8 +0,0 @@
-
-
-import '../css/altstyles.css';
-import './js/imgswap.js';
-import './js/grid.js';
-import './js/barsInsttwo.js';
-
-console.log("asdasdsadasd");
